3e609e59bb24af095b9f9eb6d9078d4fce17f3ce
Ahmed Mgua <mguaahmed@gmail.com>
2021-03-12 11:19:16 +0300

    Initial Commit

---
+++ GoogleMapsIntegration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoogleMapsIntegration
+//
+//  Created by Ahmed Mgua on 3/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoogleMapsIntegration/GoogleMapsIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapsIntegrationApp.swift
+//  GoogleMapsIntegration
+//
+//  Created by Ahmed Mgua on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMapsIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
