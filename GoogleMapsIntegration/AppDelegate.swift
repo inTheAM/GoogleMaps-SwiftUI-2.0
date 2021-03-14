3e8dddc25ab9df6cef9981a8397f7021a79d9c3c
mgua01 <68146662+mgua01@users.noreply.github.com>
2021-03-14 20:13:30 +0300

    AppDelegate.swift

--- GoogleMapsIntegration/AppDelegate.swift
+++ GoogleMapsIntegration/AppDelegate.swift
@@ -5,7 +5,6 @@ import GoogleMaps
 class AppDelegate:	NSObject,	UIApplicationDelegate	{
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 		GMSServices.provideAPIKey("INSERT_YOUR_API_KEY_HERE")
-		GMSPlacesClient.provideAPIKey("INSERT_YOUR_API_KEY_HERE")
 		return true
 	}
 }
